a19e28521e5542781916a30123a979639350a791
ElReyLocro <elibra41@hotmail.com>
2023-04-18 18:13:43 -0300

    Primer Commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan America'];
+  borrado: string = ''
+
+  borrarHeroe(): void {
+    this.borrado = this.heroes.pop() ?? '';
+  };
+}
