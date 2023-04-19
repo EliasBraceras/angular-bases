d57538085ae73ac0dd1a4ba7832f27561c2e61f4
ElReyLocro <elibra41@hotmail.com>
2023-04-19 20:54:18 -0300

    Final de la seccion 5

--- src/app/dbz/pages/main-page/main-page.component.ts
+++ src/app/dbz/pages/main-page/main-page.component.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
+import { DbzService } from '../../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  constructor( private dbzService: DbzService) {}
 
-  public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000
-  }, {
-    name: 'Goku',
-    power: 9500
-  }, {
-    name: 'Vegeta',
-    power: 7500
-  }]
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  };
+
+  onDeleteCharacter( id: string): void {
+    this.dbzService.deleteCharacterById( id );
+  };
 
   onNewCharacter( character: Character ): void {
-    console.log( 'Main Page ', character);
+    this.dbzService.addCharacter( character );
   }
 
 }
